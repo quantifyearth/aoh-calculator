085fd6d01f0233315880f70a94ca3319af13d742
Michael Dales <mwd24@cam.ac.uk>
2024-12-16 08:55:27 +0000

    Make elevation cleaning ranges parameterisable

--- cleaning.py
+++ cleaning.py
@@ -4,44 +4,50 @@ import pandas as pd
 
 ELEVATION_MIN = -500
 ELEVATION_MAX = 9000
+ELEVATION_SEPERATION = 50
 
-def tidy_data(row: pd.Series) -> pd.Series:
-    """Tidy up the data as per Busana et al"""
+def tidy_data(
+    row: pd.Series,
+    elevation_max=ELEVATION_MAX,
+    elevation_min=ELEVATION_MIN,
+    elevation_seperation=ELEVATION_SEPERATION
+) -> pd.Series:
+    """Tidy up the data as per Daniele Busana et al"""
 
     # Lower elevation higher than upper elevation
     if not pd.isna(row.elevation_lower) and not pd.isna(row.elevation_upper):
         if row.elevation_lower > row.elevation_upper:
-            row.elevation_lower = ELEVATION_MIN
-            row.elevation_upper = ELEVATION_MAX
+            row.elevation_lower = elevation_min
+            row.elevation_upper = elevation_max
 
     # Missing lower and/or upper elevation
     if pd.isna(row.elevation_lower):
-        row.elevation_lower = ELEVATION_MIN
-        if not pd.isna(row.elevation_upper) and row.elevation_upper < ELEVATION_MIN:
-            row.elevation_upper = ELEVATION_MAX
+        row.elevation_lower = elevation_min
+        if not pd.isna(row.elevation_upper) and row.elevation_upper < elevation_min:
+            row.elevation_upper = elevation_max
     if pd.isna(row.elevation_upper):
-        row.elevation_upper = ELEVATION_MAX
-        if row.elevation_lower > ELEVATION_MAX:
-            row.elevation_lower = ELEVATION_MIN
+        row.elevation_upper = elevation_max
+        if row.elevation_lower > elevation_max:
+            row.elevation_lower = elevation_min
 
     # Lower elevation < -500 and/or upper elevation > 9000
-    row.elevation_lower = max(ELEVATION_MIN, row.elevation_lower)
-    row.elevation_upper = min(ELEVATION_MAX, row.elevation_upper)
+    row.elevation_lower = max(elevation_min, row.elevation_lower)
+    row.elevation_upper = min(elevation_max, row.elevation_upper)
 
     # Small difference (<50m) between lower and upper elevation
     elevation_diff = row.elevation_upper - row.elevation_lower
-    if elevation_diff < 50:
-        spare = 50 - elevation_diff
+    if elevation_diff < elevation_seperation:
+        spare = elevation_seperation - elevation_diff
         adjust = math.ceil(spare / 2)
         row.elevation_lower -= adjust
         row.elevation_upper += adjust
 
-        if row.elevation_lower < ELEVATION_MIN:
-            adjust = ELEVATION_MIN - row.elevation_lower
+        if row.elevation_lower < elevation_min:
+            adjust = elevation_min - row.elevation_lower
             row.elevation_lower += adjust
             row.elevation_upper += adjust
-        elif row.elevation_upper > ELEVATION_MAX:
-            adjust = row.elevation_upper - ELEVATION_MAX
+        elif row.elevation_upper > elevation_max:
+            adjust = row.elevation_upper - elevation_max
             row.elevation_lower -= adjust
             row.elevation_upper -= adjust
 
--- tests/test_tidying.py
+++ tests/test_tidying.py
@@ -23,3 +23,30 @@ def test_elevation_tidy(value, expected):
     row = pd.Series(value, ["elevation_lower", "elevation_upper"])
     updated = tidy_data(row)
     assert (updated.elevation_lower, updated.elevation_upper) == expected
+
+
+@pytest.mark.parametrize("value,expected",
+    [
+        ((0.0, 100.0), (0.0, 100.0)),
+        ((0.0, 1.0), (-6.0, 7.0)),
+        ((None, 1.0), (-427.0, 1.0)),
+        ((0.0, None), (0.0, 8580.0)),
+        ((10000.0, None), (-427.0, 8580.0)),
+        ((None, -600.0), (-427.0, 8580.0)),
+        ((1.0, 0.0), (-427.0, 8580.0)),
+        ((-1000.0, 1.0), (-427.0, 1.0)),
+        ((0.0, 10000.0), (0.0, 8580.0)),
+        ((1010.0, 1020.0), (1009.0, 1021.0)),
+        ((-427.0, -420.0), (-427.0, -414.0)),
+        ((-600.0, -490.0), (-427.0, -414.0)),
+    ]
+)
+def test_elevation_tidy_different_bounds(value, expected):
+    row = pd.Series(value, ["elevation_lower", "elevation_upper"])
+    updated = tidy_data(
+        row,
+        elevation_max=8580,
+        elevation_min=-427,
+        elevation_seperation=12,
+    )
+    assert (updated.elevation_lower, updated.elevation_upper) == expected
