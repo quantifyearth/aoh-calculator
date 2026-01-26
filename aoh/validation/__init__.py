67881ac1d8675b7a588e4a1c6de9a19a6a97699d
Michael Dales <mwd24@cam.ac.uk>
2026-01-26 14:30:08 +0000

    Don't require R imports for all of validation

--- aoh/validation/__init__.py
+++ aoh/validation/__init__.py
@@ -1,6 +1,1 @@
 """Validation subpackage for habitat map validation tools."""
-
-from .collate_data import collate_data
-from .validate_map_prevalence import validate_map_prevalence
-
-__all__ = ["collate_data", "validate_map_prevalence"]
